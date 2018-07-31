ff2ab4b17e6c2faee6a771ea7c23349357d5d037
Training <Training@192.168.252.163>
2018-07-31 12:44:42 +0530

    First Code Committed.

---
+++ FirstCamelProgram/src/main/java/com/org/camel/FirstCamelProgram/App.java
@@ -0,0 +1,32 @@
+package com.org.camel.FirstCamelProgram;
+
+/**
+ * Hello world!
+ *
+ */
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+public class App{
+public static void main(String[] args) throws Exception
+{
+System.out.println("Initializing the camel context...");
+CamelContext camelContext = new DefaultCamelContext();
+System.out.println("Implementing Routes......");
+camelContext.addRoutes(new RouteBuilder(){
+@Override
+public void configure() throws Exception{
+from("file:D:\\Apache_Camel_Test\\IN").
+choice()
+.when(xpath("/Order/Country='USA'")).to("file:D:\\Apache_Camel_Test\\A")
+.when(xpath("/Order/Country='UK'")).to("file:D:\\Apache_Camel_Test\\B")
+.otherwise().to("file:D:\\Apache_Camel_Test\\C")
+.end();
+}
+});
+System.out.println("Starting Camel Context......");
+camelContext.start();
+      Thread.sleep(3000);
+      camelContext.stop();
+}
+}
